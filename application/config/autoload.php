6a4236dbad9638720fd23b681a220db110cd81b3
Iwon Gunawan <iwongunawan@gmail.com>
2020-06-07 17:48:19 +0700

    added form in helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'html', 'global', 'config');
+$autoload['helper'] = array('url', 'html', 'global', 'config', 'form');
 
 /*
 | -------------------------------------------------------------------
